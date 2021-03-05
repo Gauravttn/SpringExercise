a9647447a16ab8b98e9fbb87bc4a9a10ee11fdc1
Gauravttn <78490121+Gauravttn@users.noreply.github.com>
2021-03-05 14:24:06 +0530

    Spring Exercise

--- LooselyCoupled/SelectionSort.java
+++ LooselyCoupled/SelectionSort.java
@@ -2,7 +2,7 @@ package com.bootcamp.spring.firstspring.LooselyCoupled;
 
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
-@Primary // We use @Primary for resolving the conflict while calling the constructor of binarySearch class
+@Primary // We use @Primary for resolving the conflict while calling the constructor of BinarySearch class
 @Component
 public class SelectionSort implements Sorting{
 
