cdff785df41ae9c1cd891cf496ddf6723915ca1e
Gaurav <gk0gauravkumar@gmail.com>
2021-03-05 12:41:14 +0530

    Q5

--- LooselyCoupled/SelectionSort.java
+++ LooselyCoupled/SelectionSort.java
@@ -1,7 +1,8 @@
 package com.bootcamp.spring.firstspring.LooselyCoupled;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
-
+@Primary
 @Component
 public class SelectionSort implements Sorting{
 
